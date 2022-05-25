a2f2d2f2d7126b7d88099454fcf722b75d24f5f0
zolanovich <avilajuan0301@gmail.com>
2022-05-24 22:20:09 -0400

    Substraction developed

--- Session 3/Temperature.py
+++ Session 3/Temperature.py
@@ -29,3 +29,14 @@ class Temperature(Scale):
         else:
             return self.value
 
+    def Substract(self, temp2: Temperature):
+        diff = self.value - temp2.value
+        if(diff == 0):
+            resTemp = Temperature("Result cannot be 0", "E")
+            return resTemp
+        elif(self.scale == temp2.scale):
+            resTemp = Temperature(diff, self.scale)
+            return resTemp
+        else: 
+            resTemp = Temperature(Temperature.errString, "E")
+            return resTemp
--- Session 3/Tests/test.py
+++ Session 3/Tests/test.py
@@ -23,5 +23,29 @@ class TestCases(unittest.TestCase):
         temp2 = Temperature(-32, Temperature.Celcius)
         self.assertEqual(temp1.Add(temp2).TempToString(),"Result cannot be 0")
 
+    def test_Substraction1(self):
+        temp1 = Temperature(12, Temperature.Celcius)
+        temp2 = Temperature(13, Temperature.Celcius)
+        self.assertEqual(temp1.Substract(temp2).TempToString(),"-1 C")
+    def test_Substraction2(self):
+        temp1 = Temperature(70, Temperature.Fahrenheit)
+        temp2 = Temperature(50, Temperature.Fahrenheit)
+        self.assertEqual(temp1.Substract(temp2).TempToString(),"20 F")
+    def test_Substraction3(self):
+        temp1 = Temperature(320, Temperature.Kelvin)
+        temp2 = Temperature(280, Temperature.Kelvin)
+        self.assertEqual(temp1.Substract(temp2).TempToString(),"40 K")
+    def test_Substraction4(self):
+        temp1 = Temperature(546, Temperature.Kelvin)
+        temp2 = Temperature(52, Temperature.Fahrenheit)
+        self.assertEqual(temp1.Substract(temp2).TempToString(),Temperature.errString)
+    def test_Substraction5(self):
+        temp1 = Temperature(32, Temperature.Celcius)
+        temp2 = Temperature(32, Temperature.Celcius)
+        self.assertEqual(temp1.Substract(temp2).TempToString(),"Result cannot be 0")
+    def test_Substraction6(self):
+        temp1 = Temperature(32, Temperature.Celcius)
+        temp2 = Temperature(-32, Temperature.Celcius)
+        self.assertEqual(temp1.Substract(temp2).TempToString(),"64 C")
 
 
